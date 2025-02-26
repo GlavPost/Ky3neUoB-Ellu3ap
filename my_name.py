c079d8c634bef2b5c2e53e087fce7f4371839196
GlavPost <mr.mastik31@gmail.com>
2025-02-26 11:58:06 +0300

    Richard

--- my_name.py
+++ my_name.py
@@ -1,3 +1,7 @@
 match input("Кто ты, воин?"):
+    case "pucbka":
+        print("tbI pucbka")
+    case "писька":
+        print("tbI pucbka")
     case _:
         print("НЕ ГОВОРИ ТАК!", "НЕ ГОВОРИИИИ ТАААК!!", sep='\n')
